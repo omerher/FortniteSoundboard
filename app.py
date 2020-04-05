8c9e8e4fa1969f3d2c24cbf9f03a8afa518d74ba
Omer Hershkovitz <hershkovitzomer@gmail.com>
2020-04-05 12:07:21 -0500

    added file submit feature

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
+from werkzeug.utils import secure_filename
 import os
+from os.path import join, dirname, realpath
+
+UPLOADS_PATH = join(dirname(realpath(__file__)), 'static/uploads')
 
 path = "static/sounds/"
 players = os.listdir(path)
@@ -7,7 +11,11 @@ sounds_players = {}
 for player in players:
     sounds_players[player] = os.listdir(path + player)
 
+ALLOWED_EXTENSTIONS = {'mp3', 'wav'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOADS_PATH
+app.secret_key = b'_6mew"HES3wd/4xec]/'
 
 @app.route('/')
 def index():
@@ -25,6 +33,20 @@ def about():
 def search():
     query = request.form["search"]
     return render_template("index.html", players=sounds_players, query=query)
+
+@app.route('/submit')
+def submit():
+    return render_template("submit.html")
+
+@app.route('/submit-file', methods=["GET", "POST"])
+def submitfile():
+    if request.method == "POST":
+        file = request.files["file"]
+        player_name = request.form["name"]
+        filename = secure_filename(file.filename)
+        filename = player_name + "_" + filename  # formats the file name
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
